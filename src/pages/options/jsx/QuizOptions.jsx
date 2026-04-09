a107be101966f22451a1d2e04e1df13a8b883445
michael rose <michaelrose83@bigpond.com>
2026-04-09 10:01:47 +0800

    Changed CSS for qptions page so selections line up

--- src/pages/options/jsx/QuizOptions.jsx
+++ src/pages/options/jsx/QuizOptions.jsx
@@ -35,40 +35,38 @@ function QuizOptions() {
  };
 
  return (
-  
-   <form className="optioncontainer" onSubmit={handleSubmit}>
-    <div className="options">
-     <label className="categorycontainer">
-      Select Categorie:
-      <select
-       name="category"
-       className="categorydropbox"
-       value={inputs.category}
-       onChange={handleUserSelection}>
-       {trivia_categories.map((category) => (
-        <option key={category.id} value={category.id}>
-         {category.name}
-        </option>
-       ))}
-      </select>
-     </label>
-     <br></br>
-     <label className="difficultycontainer">
-      Select Difficulty:
-      <select
-       name="difficulty"
-       className="difficulty"
-       value={inputs.difficulty}
-       onChange={handleUserSelection}>
-       <option value="easy">Easy</option>
-       <option value="medium">Medium</option>
-       <option value="hard">Hard</option>
-      </select>
-     </label>
-     <br></br>
-     <label htmlFor="number" className="numberbanner">
-      Number of Questions:
-     </label>
+  <form className="optioncontainer" onSubmit={handleSubmit}>
+   <div className="options">
+    <label className="categorycontainer">
+     Select Categorie:
+     <select
+      name="category"
+      className="categorydropbox"
+      value={inputs.category}
+      onChange={handleUserSelection}>
+      {trivia_categories.map((category) => (
+       <option key={category.id} value={category.id}>
+        {category.name}
+       </option>
+      ))}
+     </select>
+    </label>
+    <br></br>
+    <label className="difficultycontainer">
+     Select Difficulty:
+     <select
+      name="difficulty"
+      className="difficulty"
+      value={inputs.difficulty}
+      onChange={handleUserSelection}>
+      <option value="easy">Easy</option>
+      <option value="medium">Medium</option>
+      <option value="hard">Hard</option>
+     </select>
+    </label>
+    <br></br>
+    <label htmlFor="number" className="numberbanner">
+     Number of Questions:
      <input
       id="number"
       type="number"
@@ -78,26 +76,26 @@ function QuizOptions() {
       max="25"
       value={inputs.number}
       onChange={handleUserSelection}></input>
-     <br></br>
-     <label className="modecontainer">
-      Choose Quiz Mode:
-      <select
-       name="quizMode"
-       className="quizzMode"
-       value={inputs.quizMode}
-       onChange={handleUserSelection}>
-       <option value="multiple">Multiple Choice</option>
-       <option value="boolean">True / false</option>
-       <option value="">Mix of Both</option>
-      </select>
-     </label>
-    </div>
+    </label>
     <br></br>
-    <button className="submitbtn" type="submit">
-     Start the Quiz
-    </button>
-   </form>
-
+    <label className="modecontainer">
+     Choose Quiz Mode:
+     <select
+      name="quizMode"
+      className="quizzMode"
+      value={inputs.quizMode}
+      onChange={handleUserSelection}>
+      <option value="multiple">Multiple Choice</option>
+      <option value="boolean">True / false</option>
+      <option value="">Mix of Both</option>
+     </select>
+    </label>
+   </div>
+   <br></br>
+   <button className="submitbtn" type="submit">
+    Start the Quiz
+   </button>
+  </form>
  );
 }
 
